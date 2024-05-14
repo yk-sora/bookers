7f1d64d8169154e3fd33d5ed449fdd61a7ac3a94
EC2 Default User <ec2-user@ip-172-31-19-6.ap-northeast-1.compute.internal>
2024-05-14 08:45:15 +0000

    [Finish] RSpecテスト実施

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :show
+      @books = Book.all
+      render :index
     end
   end
       
